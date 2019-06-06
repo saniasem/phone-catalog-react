a00d4ac01799df51bffaac8c6d68c0d4b5d9e10a
Oleksandr Semeniuk <olexandr.semenyuk@gmail.com>
2019-06-06 21:36:34 +0300

    Updates

---
+++ precache-manifest.03fc5d165dccb620ff34439bdcf6e5d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9d5f63325a5c1ca9db5f39b45158f9",
+    "url": "/phone-catalog-react/index.html"
+  },
+  {
+    "revision": "2b2b6265feb888f190a0",
+    "url": "/phone-catalog-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/phone-catalog-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2b2b6265feb888f190a0",
+    "url": "/phone-catalog-react/static/js/main.404c8e9c.chunk.js"
+  },
+  {
+    "revision": "4c4ec42ad940cc5b5ed1",
+    "url": "/phone-catalog-react/static/js/runtime~main.fda6b73d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phone-catalog-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
